1ceef0ae168632650ee263de18c0ab1f1a8909f5
stqinmi-Tong <stqinmi@gmail.com>
2026-01-09 00:48:36 +0800

    Update P2E.py

--- P2E.py
+++ P2E.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class CPC_word(nn.Module):  ####输入流数据是每一个批次的每个路径的关系，实体，mask和负采样实体
+class CPC_word(nn.Module):  
     def __init__(self, ent_vocab, rel_vocab, embedding_size, ar_dim, window, k_size, path_length):
         super(CPC_word, self).__init__()
         # load parameters
@@ -16,12 +16,10 @@ class CPC_word(nn.Module):  ####输入流数据是每一个批次的每个路径
         self.embedding_size = embedding_size
         # self.enc_dimension = config.cpc_model.enc_dim
         self.ar_dimension = ar_dim
-        self.k_size = k_size  ###指的窗口大小,我们选取的路径长度为5，有两跳，想先选k=3试试，从第一个三元组的尾实体开始测试
+        self.k_size = k_size  
         self.epsilon = 2.0
         gamma = 24.0
-        # define embedding layer
-        # self.ent_embedding = nn.Embedding(self.ent_vocab, self.embedding_size)
-        # self.rel_embedding = nn.Embedding(self.rel_vocab, self.embedding_size)
+       
         self.gamma = nn.Parameter(
             torch.Tensor([gamma]),
             requires_grad=False
@@ -48,14 +46,12 @@ class CPC_word(nn.Module):  ####输入流数据是每一个批次的每个路径
         if self.window != self.path_length:
             self.window = self.path_length
 
-        # define autoregressive model: gar,输入维度为enc_dimension的向量得到维度为ar_dimension的结果向量
-        # 模型待定，先试试RNN
         self.gru = nn.GRU(
             # self.enc_dimension,
             self.embedding_size,
             self.ar_dimension,
             batch_first=True)
-        # define predictive layer: W2
+      
         self.Wk = nn.ModuleList([nn.Linear(self.ar_dimension, self.embedding_size, bias=False)
                                  for i in range(self.k_size)])  # shape：（self.enc_dimension, self.ar_dimension）
 
@@ -66,14 +62,11 @@ class CPC_word(nn.Module):  ####输入流数据是每一个批次的每个路径
             return torch.zeros(1, batch_size, self.ar_dimension)
 
     # B: Batch, W: Window, S: Sentence, D: Dimension
-    def forward(self, ents, rels, device):  # x.shape: (B,W,S) ####输入流数据是每一个批次的每个路径的关系，实体，mask和负采样实体
-
-        # print(ents)
+    def forward(self, ents, rels, device):  # x.shape: (B,W,S) 
         batch = ents.shape[0]
         ent_num = self.path_length // 2 + 1
         rel_num = self.path_length // 2
         hidden = self.init_hidden(batch, device)  # hidden.shape:(B,D) D(default:2400)
-        # print(ents.shape)#torch.Size([128, 3])
 
         # get sentence embeddings
         z = self.get_input_embedding(batch, ents.view(batch * ent_num),
@@ -83,17 +76,17 @@ class CPC_word(nn.Module):  ####输入流数据是每一个批次的每个路径
 
         # separate forward and target samples  W1: forward window, W2: target window
         target = z[:, -self.k_size:, :].transpose(0, 1)  # target.shape: (W2,B,D)
-        forward_sequence = z[:, :-self.k_size,:]  # forward_sequence.shape: (B,W1,D)，，W1+W2=len，前W1是输入，后W2是要做验证的输出，len=W
+        forward_sequence = z[:, :-self.k_size,:]  # forward_sequence.shape: (B,W1,D)
         # feed ag model
         self.gru.flatten_parameters()
         output, hidden = self.gru(forward_sequence, hidden)  # output.shape: (B,W1,D) 也即ct-3，ct-2，ct-1，ct
-        context = output[:, -1, :].view(batch,self.ar_dimension)  # context.shape: (B,D) (take last hidden state) 也即把ct取出来，这里D=self.ar_dimension，torch.Size([64, 32])
+        context = output[:, -1, :].view(batch,self.ar_dimension)  # context.shape: (B,D) (take last hidden state) 
         pred = torch.empty((self.k_size, batch, self.embedding_size), dtype=torch.float, device=device)  # pred (empty container).shape: (W2,B,D)
 
         # loop of prediction
         for i in range(self.k_size):
             linear = self.Wk[i]  # （self.embedding_size, self.ar_dimension） Linear(in_features=32, out_features=64, bias=True)  context: torch.Size([64, 32])
-            pred[i] = linear(context)  # Wk*context.shape: (B,D) pred[i].shape:(B,self.embedding_size),pred.shape: (k_size,B,self.embedding_size)也即（W2，B，D）
+            pred[i] = linear(context)  # Wk*context.shape: (B,D) pred[i].shape:(B,self.embedding_size),pred.shape: (k_size,B,self.embedding_size):（W2，B，D）
 
         loss, accuracy = self.info_nce(pred, target)  # shape: (W2,B,D)
 
@@ -125,18 +118,6 @@ class CPC_word(nn.Module):  ####输入流数据是每一个批次的每个路径
                     paths_embed.append(path_embed)
                     path_embed = []
 
-        # for i,x in enumerate(ent_embed):
-        #     # print(x.shape)
-        #     for j,y in enumerate(x):
-        #         path_embed.append(ent_embed[i][j])
-        #     for t,y in enumerate(x):
-        #         if len(path_embed) != self.path_length:
-        #             # print(i,t)
-        #             path_embed.append(rel_embed[i][t])
-        #         else:
-        #             paths_embed.append(path_embed)
-        #             path_embed = []
-
         inputs_embedding = []
         for i in paths_embed:
             input_embedding = torch.cat([x for x in i], 0).reshape(5, -1).squeeze(0)
@@ -155,7 +136,7 @@ class CPC_word(nn.Module):  ####输入流数据是每一个批次的每个路径
             target.reshape([-1, hidden_size]).transpose(-1, -2)
         )
 
-        loss = nn.functional.cross_entropy(logits, label, reduction='none')   ###不用对label进行one_hot编码，因为nn.functional.cross_entropy函数中的nll_loss函数已经实现了类似one-hot过程，不同之处是当class = [1, 2, 3]时要处理成从0开始[0, 1, 2]。
+        loss = nn.functional.cross_entropy(logits, label, reduction='none')   
         accuracy = torch.eq(torch.argmax(F.softmax(logits, dim=1), dim=1), label)
 
         # process for split loss and accuracy into k pieces (useful for logging)
@@ -200,27 +181,5 @@ class TxtClassifier(nn.Module):
         x = self.classifier(x)
         return F.log_softmax(x, dim=-1)
 
-# if __name__ =='__main__':
-#     print('=====CPC testing=====')
-#     gpu=0
-#     if gpu != '-1' and torch.cuda.is_available():
-#         os.environ["CUDA_VISIBLE_DEVICES"] = '0,1,2,3'
-#         torch.cuda.set_device(2)
-#         device = torch.device("cuda:2" if torch.cuda.is_available() else "cpu")
-#         torch.cuda.set_rng_state(torch.cuda.get_rng_state())
-#         torch.backends.cudnn.deterministic = True
-#     else:
-#        device = torch.device('cpu')
-#     ents = torch.ones([128, 3]).long().to(device)
-#     rels = torch.ones([128, 2]).long().to(device)
-#     # gru_ouput = torch.zeros([64, 300, 2]).to(device)
-#     # ent_negs = torch.zeros([64, 3, 20]).long().to(device)
-#     ent_negs = None
-#     # (self, ent_vocab, rel_vocab, embedding_size,ar_dim, window, k_size,path_length)
-#     model = CPC(60, 6, 100, 100, 5, 2, 5).to(device)
-#     loss, accuracy = model(ents, rels, ent_negs, device)  # forward(self, ents, rels)
-#     print('loss', loss)  # loss tensor([[5.5461, 5.2585, 6.1626]], device='cuda:0',grad_fn = < UnsqueezeBackward0 >)
-#     print('accuracy', accuracy)  # accuracy tensor([[0.0078, 0.0078, 0.0000]], device='cuda:0')
-#
-#     # txt_model = TxtClassifier(config).to(device)
+
 
